267b7329fd647cb52dce7c79d6abb647856f7798
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2024-05-09 21:28:05 +0900

    chore: Add @radix-ui/react-alert-dialog, @radix-ui/react-checkbox, @radix-ui/react-scroll-area, and @tanstack/react-table packages, update dependencies

---
+++ app/components/breadcrumb.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "@remix-run/react";
+import { ChevronRightIcon } from "@radix-ui/react-icons";
+
+import { cn } from "~/lib/utils";
+
+type BreadCrumbType = {
+  title: string;
+  link: string;
+};
+
+type BreadCrumbPropsType = {
+  items: BreadCrumbType[];
+};
+
+export default function BreadCrumb({ items }: BreadCrumbPropsType) {
+  return (
+    <div className="mb-4 flex items-center space-x-1 text-sm text-muted-foreground">
+      <Link
+        to={"/dashboard/home"}
+        className="overflow-hidden text-ellipsis whitespace-nowrap"
+      >
+        Dashboard
+      </Link>
+      {items?.map((item: BreadCrumbType, index: number) => (
+        <React.Fragment key={item.title}>
+          <ChevronRightIcon className="h-4 w-4" />
+          <Link
+            to={item.link}
+            className={cn(
+              "font-medium",
+              index === items.length - 1
+                ? "pointer-events-none text-foreground"
+                : "text-muted-foreground",
+            )}
+          >
+            {item.title}
+          </Link>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
