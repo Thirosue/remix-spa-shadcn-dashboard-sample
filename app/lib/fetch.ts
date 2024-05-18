dae389c3b06b5172afd19ef4b3a2bc6b49719d27
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2024-05-18 14:41:46 +0900

    refactor: Add optional headers parameter to postData function

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -1,6 +1,6 @@
 const BASE_URL = "https://next-typescript-sample-mu.vercel.app";
 
-export async function postData(url: string, data = {}) {
+export async function postData(url: string, data = {}, headers = {}) {
   // 既定のオプションには * が付いています
   const response = await fetch(`${BASE_URL}${url}`, {
     method: "POST", // *GET, POST, PUT, DELETE, etc.
@@ -8,6 +8,7 @@ export async function postData(url: string, data = {}) {
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     headers: {
       "Content-Type": "application/json",
+      ...headers,
     },
     body: JSON.stringify(data), // 本体のデータ型は "Content-Type" ヘッダーと一致させる必要があります
   });
