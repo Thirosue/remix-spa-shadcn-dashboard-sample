b859c0f8bee7b19378509f37dbbd83f88b04e2ad
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2024-05-20 10:52:22 +0900

    feat: Add headers parameter to getData, putData, and deleteData functions

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -15,19 +15,20 @@ export async function postData(url: string, data = {}, headers = {}) {
   return response.json(); // JSON のレスポンスをネイティブの JavaScript オブジェクトに解釈
 }
 
-export async function getData(url: string) {
+export async function getData(url: string, headers = {}) {
   const response = await fetch(`${BASE_URL}${url}`, {
     method: "GET", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     headers: {
       "Content-Type": "application/json",
+      ...headers,
     },
   });
   return response.json(); // JSON のレスポンスをネイティブの JavaScript オブジェクトに解釈
 }
 
-export async function putData(url: string, data = {}) {
+export async function putData(url: string, data = {}, headers = {}) {
   // 既定のオプションには * が付いています
   const response = await fetch(`${BASE_URL}${url}`, {
     method: "PUT", // *GET, POST, PUT, DELETE, etc.
@@ -35,13 +36,14 @@ export async function putData(url: string, data = {}) {
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     headers: {
       "Content-Type": "application/json",
+      ...headers,
     },
     body: JSON.stringify(data), // 本体のデータ型は "Content-Type" ヘッダーと一致させる必要があります
   });
   return response.json(); // JSON のレスポンスをネイティブの JavaScript オブジェクトに解釈
 }
 
-export async function deleteData(url: string) {
+export async function deleteData(url: string, headers = {}) {
   // 既定のオプションには * が付いています
   const response = await fetch(`${BASE_URL}${url}`, {
     method: "DELETE", // *GET, POST, PUT, DELETE, etc.
@@ -49,6 +51,7 @@ export async function deleteData(url: string) {
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     headers: {
       "Content-Type": "application/json",
+      ...headers,
     },
   });
   return response.json(); // JSON のレスポンスをネイティブの JavaScript オブジェクトに解釈
